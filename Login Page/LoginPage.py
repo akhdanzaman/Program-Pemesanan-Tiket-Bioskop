d6e099d77b3e8c523f20100fdd5f7ace2d3497a2
DanishAdinata <danishfaizi2003@student.uns.ac.id>
2022-06-09 18:17:03 +0700

    Window Utama Login

--- Login Page/LoginPage.py
+++ Login Page/LoginPage.py
@@ -13,7 +13,7 @@ class LoginPage:
         # ========================================================================
         # ============================background image============================
         # ========================================================================
-        self.bg_frame = Image.open('images\\M5n.png')
+        self.bg_frame = Image.open('images\\M6new.png')
         photo = ImageTk.PhotoImage(self.bg_frame)
         self.bg_panel = Label(self.window, image=photo)
         self.bg_panel.image = photo
@@ -28,7 +28,7 @@ class LoginPage:
         # ========================================================================
         self.txt = "Cinemakmur Premiere"
         self.heading = Label(self.lgn_frame, text=self.txt, font=('yu gothic ui', 25, "bold"), bg="#040405",
-                             fg='White',
+                             fg='Gold',
                              bd=5,
                              relief=FLAT)
         self.heading.place(x=50, y=30, width=400, height=30)
@@ -37,7 +37,7 @@ class LoginPage:
         # ========================================================================
         # ============ Left Side Image ================================================
         # ========================================================================
-        self.side_image = Image.open('images\\G.png')
+        self.side_image = Image.open('images\\Gnew1.png')
         photo = ImageTk.PhotoImage(self.side_image)
         self.side_image_label = Label(self.lgn_frame, image=photo, bg='#040405')
         self.side_image_label.image = photo
@@ -47,23 +47,23 @@ class LoginPage:
         # ========================================================================
         # ============ Sign In label =============================================
         # ========================================================================
-        self.sign_in_label = Label(self.lgn_frame, text="Sign In", bg="#040405", fg="white",
+        self.sign_in_label = Label(self.lgn_frame, text="Sign In", bg="#040405", fg="Gold",
                                     font=("yu gothic ui", 17, "bold"))
-        self.sign_in_label.place(x=550, y=240)
+        self.sign_in_label.place(x=550, y=140)
 
         # ========================================================================
         # ============================username====================================
         # ========================================================================
         self.username_label = Label(self.lgn_frame, text="Username", bg="#040405", fg="#4f4e4d",
                                     font=("yu gothic ui", 13, "bold"))
-        self.username_label.place(x=550, y=300)
+        self.username_label.place(x=550, y=200)
 
         self.username_entry = Entry(self.lgn_frame, highlightthickness=0, relief=FLAT, bg="#040405", fg="#6b6a69",
                                     font=("yu gothic ui ", 12, "bold"))
-        self.username_entry.place(x=550, y=335, width=270)
+        self.username_entry.place(x=550, y=235, width=270)
 
         self.username_line = Canvas(self.lgn_frame, width=300, height=2.0, bg="#bdb9b1", highlightthickness=0)
-        self.username_line.place(x=550, y=359)
+        self.username_line.place(x=550, y=259)
     
       
 
@@ -74,7 +74,7 @@ class LoginPage:
         photo = ImageTk.PhotoImage(self.lgn_button)
         self.lgn_button_label = Label(self.lgn_frame, image=photo, bg='#040405')
         self.lgn_button_label.image = photo
-        self.lgn_button_label.place(x=550, y=450)
+        self.lgn_button_label.place(x=550, y=350)
         self.login = Button(self.lgn_button_label, text='LOGIN', font=("yu gothic ui", 13, "bold"), width=25, bd=0,
                             bg='#3047ff', cursor='hand2', activebackground='#3047ff', fg='white')
         self.login.place(x=20, y=10)
@@ -82,26 +82,26 @@ class LoginPage:
         # =========== Sign Up ==================================================
         self.sign_label = Label(self.lgn_frame, text='No account yet?', font=("yu gothic ui", 11, "bold"),
                                 relief=FLAT, borderwidth=0, background="#040405", fg='white')
-        self.sign_label.place(x=550, y=560)
+        self.sign_label.place(x=550, y=460)
 
         self.signup_img = ImageTk.PhotoImage(file='images\\register.png')
         self.signup_button_label = Button(self.lgn_frame, image=self.signup_img, bg='#98a65d', cursor="hand2",
                                           borderwidth=0, background="#040405", activebackground="#040405")
-        self.signup_button_label.place(x=670, y=555, width=111, height=35)
+        self.signup_button_label.place(x=670, y=455, width=111, height=35)
 
         # ========================================================================
         # ============================password====================================
         # ========================================================================
         self.password_label = Label(self.lgn_frame, text="Password", bg="#040405", fg="#4f4e4d",
                                     font=("yu gothic ui", 13, "bold"))
-        self.password_label.place(x=550, y=380)
+        self.password_label.place(x=550, y=280)
 
         self.password_entry = Entry(self.lgn_frame, highlightthickness=0, relief=FLAT, bg="#040405", fg="#6b6a69",
                                     font=("yu gothic ui", 12, "bold"), show="*")
-        self.password_entry.place(x=550, y=416, width=244)
+        self.password_entry.place(x=550, y=316, width=244)
 
         self.password_line = Canvas(self.lgn_frame, width=300, height=2.0, bg="#bdb9b1", highlightthickness=0)
-        self.password_line.place(x=550, y=440)
+        self.password_line.place(x=550, y=340)
       
         # ========= show/hide password ==================================================================
         self.show_image = ImageTk.PhotoImage \
@@ -113,20 +113,20 @@ class LoginPage:
         self.show_button = Button(self.lgn_frame, image=self.show_image, command=self.show, relief=FLAT,
                                   activebackground="white"
                                   , borderwidth=0, background="white", cursor="hand2")
-        self.show_button.place(x=860, y=420)
+        self.show_button.place(x=860, y=320)
 
     def show(self):
         self.hide_button = Button(self.lgn_frame, image=self.hide_image, command=self.hide, relief=FLAT,
                                   activebackground="white"
                                   , borderwidth=0, background="white", cursor="hand2")
-        self.hide_button.place(x=860, y=420)
+        self.hide_button.place(x=860, y=320)
         self.password_entry.config(show='')
 
     def hide(self):
         self.show_button = Button(self.lgn_frame, image=self.show_image, command=self.show, relief=FLAT,
                                   activebackground="white"
                                   , borderwidth=0, background="white", cursor="hand2")
-        self.show_button.place(x=860, y=420)
+        self.show_button.place(x=860, y=320)
         self.password_entry.config(show='*')
 
 
