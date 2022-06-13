096713d43b2b54f4531b1b319efa79883180c3d6
akhdanzaman <akhdanzaman@student.uns.ac.id>
2022-06-13 15:35:50 +0700

    upload pilih kursi

--- tes2.py
+++ tes2.py
@@ -17,31 +17,33 @@ def cariindekskursi():
     datakursi2 = pd.read_csv('datakursi2.csv')
     carkur3 = pd.DataFrame(datakursi2)
     carkur2 = carkur3[carkur3['judul']=="ARCHER"]
-    carkur = carkur2[carkur2['tanggal']==22]
+    carkur = carkur2[carkur2['tanggal']==12]
     indekskursi = carkur[carkur['jam']==10].index.item()
 
 def makebutton(avaibility,seatno,col,row):
+    bariskursi=['A','B','C',"D","E","F","G"]
     seat_code=int(seatno-6)
     lgn_button = Image.open('E:\Files\Tugasnip\KULIAH\Prokom\Program-Pemesanan-Tiket-Bioskop\Login Page\images\\btn1 - Copy.png')
     photo = ImageTk.PhotoImage(lgn_button)
-    lgn_button_label = Label(root, image=photo, bg='white')
+    lgn_button_label = Label(root, bg='white')
     lgn_button_label.image = photo
-    lgn_button_label.grid(column=col,row=row,padx=5,pady=5)
+    lgn_button_label.grid(column=col,row=row,padx=1,pady=1)
     login = Button(lgn_button_label, 
-                    text='SEAT %d' %seat_code, font=("yu gothic ui", 13, "bold"), 
+                    text='%s%d' %(bariskursi[row],col), font=("yu gothic ui", 9, "bold"), 
                     width=5, bd=0,
-                    bg='#3047ff',
+                    bg='#152238',
                     cursor='hand2', 
                     activebackground='#3047ff', 
                     fg='white',
-                    command =lambda:store_picked_seatno(seat_code))
+                    command =lambda:store_picked_seatno(seat_code),
+                    borderwidth=0)
     login.place(x=20, y=10)
     login.grid(column=seat_code,row=0)
     if avaibility != "ava":
         login.configure(bg='grey',state=DISABLED)
 
     if pickedseat.count(seat_code) > 0 :
-        login.configure(bg='white',command =lambda:del_picked_seatno(seat_code))
+        login.configure(bg='#3047ff',command =lambda:del_picked_seatno(seat_code))
         print("ADALUR")
 def availableseat():
     kursifilm = pd.read_csv('datakursi2.csv')
@@ -51,7 +53,7 @@ def availableseat():
     print()
     for a in range (7,len(daftarkursi)):
         row = ((a+1)//8)-1
-        column = ((a+1)-(7*row))-row
+        column = ((a+1)-(7*row))-row-7
         if daftarkursi[a] == 1:
             print('%d Available'%(a-6))
             makebutton("ava",a,column,row)
@@ -63,10 +65,10 @@ def availableseat():
     photo = ImageTk.PhotoImage(lgn_button)
     lgn_button_label = Label(root, image=photo, bg='white')
     lgn_button_label.image = photo
-    lgn_button_label.grid(column=2,row=9,padx=20,pady=20)
+    lgn_button_label.grid(column=2,row=9,padx=1,pady=1)
     login = Button(lgn_button_label, 
-                    text='SUBMIT', font=("yu gothic ui", 13, "bold"), 
-                    width=5, bd=0,
+                    text='SUBMIT', font=("yu gothic ui", 5, "bold"), 
+                    width=3, bd=0,
                     bg='#3047ff',
                     cursor='hand2', 
                     activebackground='#3047ff', 
@@ -76,11 +78,9 @@ def availableseat():
     login.grid(column=1,row=1)
 def store_picked_seatno(seat_code):
     pickedseat.append(seat_code)
-    print(pickedseat)
     availableseat()
 def del_picked_seatno(seat_code):
     pickedseat.remove(seat_code)
-    print(pickedseat)
     availableseat()
 pickedseat=[]
 def pickseat():
